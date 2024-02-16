56fc06078e4be123e0014b269d9945278e7d9a61
mathewjordan <mat@northwestern.edu>
2024-02-16 21:36:18 +0000

    Deploying to gh-pages from @ mathewjordan/monroe-county-history@2f7ff57b2cd6d3cac39aff1871b4f1e0ec43e721 🚀

---
+++ _next/static/chunks/881.1d47550a48fc43da.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[881],{8881:function(t){t.exports=JSON.parse('{"collection":"https://raw.githubusercontent.com/markpbaggett/monroe-county-history/main/collection.json"}')}}]);
